8045b5fb90e6c566d7ff5ead173dc37b6bb1ba8e
Christofer Chan <devchumbing@gmail.com>
2018-06-14 12:08:53 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,10 +1,11 @@
 # REMEMBER: print your output to the terminal using 'puts'
-counter = 0
-def loop_iterator(number_of_times)
 
-  while counter < number_of_times
+def loop_iterator(number_of_times)
+counter = 0
+  loop do
     counter += 1
     phrase = "Welcome to Flatiron School's Web Development Course!"
+    break if counter < number_of_times
 end
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer
